7824a3e8945d2d481e18e101aa0fcc3e90ddcdc0
Tóth Péter <peeter.toth@gmail.com>
2016-07-28 14:56:07 +0200

    változó neve el volt írva (nem lett kijavítva)

--- ncore-torrent-lapozas.user.js
+++ ncore-torrent-lapozas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ncore-torrent-lapozas
 // @namespace    peetftp.ddns.net
-// @version      1.1
+// @version      1.2
 // @description  torrent oldalon lehet billentyűkkel lapozni (,.-) leírás: https://github.com/peetertoth/ncore-torrent-lapozas/blob/master/README.md
 // @author       kyle
 // @match        https://ncore.cc/torrents.php*
